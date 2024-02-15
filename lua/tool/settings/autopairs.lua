33e368be3bfa017b35a8560ea5aaad66950d9a06
wukuohao <wukuohao2022@icloud.com>
2024-02-15 21:24:09 +0800

    cmp add auto-pairs

--- lua/tool/settings/autopairs.lua
+++ lua/tool/settings/autopairs.lua
@@ -1,5 +1,7 @@
 local M = {}
 local status, atpairs = pcall(require, "nvim-autopairs")
+local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+local cmp = require("cmp")
 
 if not status then
 	vim.notify("auto-pairs is not found ...")
@@ -8,6 +10,7 @@ end
 
 function M.Config()
 	atpairs.setup()
+	cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
 end
 
 return M
