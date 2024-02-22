661b93b634b9e507a1058ab2b5d639ab359fd59f
wukuohao <wukuohao2022@icloud.com>
2024-02-22 09:09:49 +0800

    change plugin file name

--- init.lua
+++ init.lua
@@ -2,26 +2,26 @@ local modules = {
 	{ moduleName = "conf.commands", enabled = true },
 	{ moduleName = "conf.keymaps", enabled = true },
 	{ moduleName = "conf.custom", enabled = true },
-	{ moduleName = "tool.package", enabled = true },
-	{ moduleName = "tool.tool-list.icons", enabled = true },
-	{ moduleName = "tool.tool-list.lualine", enabled = true },
-	{ moduleName = "tool.tool-list.nvim-tree", enabled = true },
-	{ moduleName = "tool.tool-list.tree-sitter", enabled = true },
-	{ moduleName = "tool.tool-list.telescope", enabled = true },
-	{ moduleName = "tool.tool-list.formatter", enabled = true },
-	{ moduleName = "tool.tool-list.floaterm", enabled = true },
-	{ moduleName = "tool.tool-list.transparent", enabled = true },
-	{ moduleName = "tool.tool-list.notify", enabled = true },
-	{ moduleName = "tool.tool-list.tundra", enabled = true },
-	{ moduleName = "tool.tool-list.autopairs", enabled = true },
-	{ moduleName = "tool.tool-list.commenter", enabled = true },
+	{ moduleName = "util.package", enabled = true },
+	{ moduleName = "util.util-list.icons", enabled = true },
+	{ moduleName = "util.util-list.lualine", enabled = true },
+	{ moduleName = "util.util-list.nvim-tree", enabled = true },
+	{ moduleName = "util.util-list.tree-sitter", enabled = true },
+	{ moduleName = "util.util-list.telescope", enabled = true },
+	{ moduleName = "util.util-list.formatter", enabled = true },
+	{ moduleName = "util.util-list.floaterm", enabled = true },
+	{ moduleName = "util.util-list.transparent", enabled = true },
+	{ moduleName = "util.util-list.notify", enabled = true },
+	{ moduleName = "util.util-list.tundra", enabled = true },
+	{ moduleName = "util.util-list.autopairs", enabled = true },
+	{ moduleName = "util.util-list.commenter", enabled = true },
 	{ moduleName = "lsp.mason.mason", enabled = true },
 	{ moduleName = "lsp.lsp", enabled = true },
 	{ moduleName = "lsp.snips.snips", enabled = true },
 }
 
-for _, tool in ipairs(modules) do
-	if tool.enabled then
-		require(tool.moduleName).Config()
+for _, util in ipairs(modules) do
+	if util.enabled then
+		require(util.moduleName).Config()
 	end
 end
