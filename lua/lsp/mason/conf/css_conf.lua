b4c7e7bf65791ec14587200e64a49254d9271a06
wukuohao <wukuohao2022@icloud.com>
2024-02-23 20:23:40 +0800

    change css lsp setting

--- lua/lsp/mason/conf/css_conf.lua
+++ lua/lsp/mason/conf/css_conf.lua
@@ -1,7 +1,12 @@
 local M = {}
 
 function M.CssLSP()
-	require("lspconfig").cssls.setup({})
+	local capabilities = vim.lsp.protocol.make_client_capabilities()
+	capabilities.textDocument.completion.completionItem.snippetSupport = true
+
+	require("lspconfig").cssls.setup({
+		capabilities = capabilities,
+	})
 end
 
 return M
