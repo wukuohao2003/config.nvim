769cf16b190364cd57e68346064947299fdb4f0d
wukuohao <wukuohao2022@icloud.com>
2024-02-25 20:06:12 +0800

    change emmet_language_server conf

--- lua/lsp/mason/conf/emmet_conf.lua
+++ lua/lsp/mason/conf/emmet_conf.lua
@@ -1,7 +1,15 @@
 local M = {}
 
 function M.EmmetLSP()
-	require("lspconfig").emmet_ls.setup({})
+	require("lspconfig").emmet_ls.setup({
+		filetypes = {
+			"html",
+			"htmldjango",
+			"javascriptreact",
+			"typescriptreact",
+			"vue",
+		},
+	})
 end
 
 return M
