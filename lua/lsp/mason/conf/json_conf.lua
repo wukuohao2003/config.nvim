eba912b1a7cec1633497da634d467f8c03566379
wukuohao <wukuohao2022@icloud.com>
2024-02-25 20:17:34 +0800

    change json_language_server conf

--- lua/lsp/mason/conf/json_conf.lua
+++ lua/lsp/mason/conf/json_conf.lua
@@ -1,7 +1,12 @@
 local M = {}
 
 function M.JsonLSP()
-	require("lspconfig").jsonls.setup({})
+	local capabilities = vim.lsp.protocol.make_client_capabilities()
+	capabilities.textDocument.completion.completionItem.snippetSupport = true
+
+	require("lspconfig").jsonls.setup({
+		capabilities = capabilities,
+	})
 end
 
 return M
