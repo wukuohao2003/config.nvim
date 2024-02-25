b2231d655c1c6d7b5bc2c84095b4527abfbc856c
wukuohao <wukuohao2022@icloud.com>
2024-02-25 20:23:23 +0800

    change html_language_server conf

--- lua/lsp/mason/conf/html_conf.lua
+++ lua/lsp/mason/conf/html_conf.lua
@@ -1,7 +1,12 @@
 local M = {}
 
 function M.HtmlLSP()
-	require("lspconfig").html.setup({})
+	local capabilities = vim.lsp.protocol.make_client_capabilities()
+	capabilities.textDocument.completion.completionItem.snippetSupport = true
+
+	require("lspconfig").html.setup({
+		capabilities = capabilities,
+	})
 end
 
 return M
